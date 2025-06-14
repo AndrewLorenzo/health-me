e38bdfb20cf64313d3c6cb2e9fca69d8520d892a
AndrewLorenzo <vincentius.lorenzo@binus.ac.id>
2025-06-14 22:17:53 +0700

    Update database.sql
    cascade

--- database.sql
+++ database.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE HealthMe;
 USE HealthMe;
+
 CREATE TABLE UsersAccount (
-	UserID int PRIMARY KEY AUTO_INCREMENT,
+    UserID int PRIMARY KEY AUTO_INCREMENT,
     Email varchar(255),
     Pass varchar(255)
 );
+
 CREATE TABLE UsersProfiles (
-    UserID int PRIMARY KEY AUTO_INCREMENT,
-    FirstName varchar(50),	
-    LastName varchar(50),	
-    Age int(11),	
+    UserID int PRIMARY KEY,
+    FirstName varchar(50),    
+    LastName varchar(50),    
+    Age int(11),    
     Height float,
     Weight float,
     Sex varchar(10),
     BMI float,
-    FOREIGN KEY (UserID) REFERENCES UsersAccount(UserID)
+    FOREIGN KEY (UserID) REFERENCES UsersAccount(UserID) ON DELETE CASCADE
 );
